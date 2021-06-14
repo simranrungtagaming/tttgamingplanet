1f2f70440b22396e337ec451e803e613f4529601
simranrungtagaming <kartikkrishnasharmaa@gmail.com>
2021-06-14 15:20:55 +0530

    Updates

---
+++ precache-manifest.12e1df06fa9d7e828c70c0281fde2d12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bc961849514901d8a0bbc3ad5042bd1",
+    "url": "/tttgamingplanet/index.html"
+  },
+  {
+    "revision": "f405ac038dd6d4c2f540",
+    "url": "/tttgamingplanet/static/css/main.9314aa7c.chunk.css"
+  },
+  {
+    "revision": "e1c6a816ccf14fa6bf7d",
+    "url": "/tttgamingplanet/static/js/2.9ead5ae3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/tttgamingplanet/static/js/2.9ead5ae3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f405ac038dd6d4c2f540",
+    "url": "/tttgamingplanet/static/js/main.26ee5b23.chunk.js"
+  },
+  {
+    "revision": "df5a24951286d93411a1",
+    "url": "/tttgamingplanet/static/js/runtime-main.d1a12951.js"
+  }
+]);
